89def5ae014c252f1c434205db0c4b3419179eec
loicniragire <loic.niragire@gmail.com>
2024-12-27 09:24:21 -0500

    Container names...

--- infrastructure/modules/blob_storage/output.tf
+++ infrastructure/modules/blob_storage/output.tf
@@ -6,7 +6,7 @@ output "images_primary_blob_endpoint" {
 }
 
 output "storage_container_names" {
-  value = azurerm_storage_container.containers[*].name
+  value = [for container in azurerm_storage_container.container : container.name]
 }
 
 
