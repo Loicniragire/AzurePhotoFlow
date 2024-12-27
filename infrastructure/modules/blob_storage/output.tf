263e6f0bda874224401b988f628a6352d5e4461a
loicniragire <loic.niragire@gmail.com>
2024-12-27 08:38:58 -0500

    Storage container TF

--- infrastructure/modules/blob_storage/output.tf
+++ infrastructure/modules/blob_storage/output.tf
@@ -1,7 +1,11 @@
 output "storage_account_name" {
-  value = azurerm_storage_account.storage.name
+  value = data.azurerm_storage_account.storage.name
 }
-output "images_container_name" {
+output "images_primary_blob_endpoint" {
+  value = data.azurerm_storage_account.storage.primary_blob_endpoint
+}
+
+output "storage_container_name" {
   value = azurerm_storage_container.images.name
 }
 
