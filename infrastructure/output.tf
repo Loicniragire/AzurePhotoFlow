2719fea365247fa283693373d4101382609964c2
loicniragire <loic.niragire@gmail.com>
2024-12-28 09:20:59 -0500

    Testing...

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -15,10 +15,10 @@ output "container_registry_login_server" {
 
 output "backend_app_service_url" {
   description = "The URL of the backend App Service"
-  value       = azurerm_linux_web_app.backend.default_site_hostname
+  value       = azurerm_linux_web_app.backend.default_hostname
 }
 
 output "frontend_app_service_url" {
   description = "The URL of the frontend App Service"
-  value       = azurerm_linux_web_app.frontend.default_site_hostname
+  value       = azurerm_linux_web_app.frontend.default_hostname
 }
