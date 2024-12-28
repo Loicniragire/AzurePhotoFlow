cf190036e594e80c0fbaa6fe5d8b68a8891f9d01
loicniragire <loic.niragire@gmail.com>
2024-12-28 09:39:55 -0500

    Moving service_plan to eastus2 - Free sku

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -57,7 +57,7 @@ resource "azurerm_container_registry" "acr" {
 # Shared service plan for all App Services
 resource "azurerm_service_plan" "service_plan" {
   name                = var.service_plan_name
-  location            = var.location
+  location            = "eastus2"
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
   sku_name			  = "F1"
