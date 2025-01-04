d2b3ebadf1c252bbde36bddb5b2c95ccf8a7bc5c
loicniragire <loic.niragire@gmail.com>
2025-01-04 16:08:47 -0500

    Executing frontend from container

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import history from 'connect-history-api-fallback';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-	outDir: 'build',
+    outDir: 'build',
   },
-	server:{
-		host: 'localhost', // default 'localhost
-		port: 3000,
-		protocol: 'ws'
-	}
-})
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    middlewareMode: true, // Enable middleware
+    setupMiddleware: (app) => {
+      app.use(history()); // Redirect all navigation requests to index.html
+    },
+  },
+  base: '/', // Base URL for the app
+});
+
