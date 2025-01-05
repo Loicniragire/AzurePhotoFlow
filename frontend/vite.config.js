20129db1dfe3c8ea72ddc070a3273ee31a4274a3
loicniragire <loic.niragire@gmail.com>
2025-01-05 07:06:05 -0500

    Added a MODE argument to vite.conf to support development vs production environment variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import history from 'connect-history-api-fallback';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
     outDir: 'build',
   },
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0', // Allows access from Docker or other devices
     port: 3000,
-    middlewareMode: true, // Enable middleware
-    setupMiddleware: (app) => {
-      app.use(history()); // Redirect all navigation requests to index.html
-    },
   },
-  base: '/', // Base URL for the app
-});
+  base: mode === 'production' ? '/production/' : '/', // Example conditional base URL
+}));
 
