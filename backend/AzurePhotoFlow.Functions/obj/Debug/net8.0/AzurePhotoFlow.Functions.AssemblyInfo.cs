185c1bb62acdb730f8062384d79fe94cf20e162b
loicniragire <loic.niragire@gmail.com>
2025-01-08 10:07:03 -0500

    Testing

--- backend/AzurePhotoFlow.Functions/obj/Debug/net8.0/AzurePhotoFlow.Functions.AssemblyInfo.cs
+++ backend/AzurePhotoFlow.Functions/obj/Debug/net8.0/AzurePhotoFlow.Functions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzurePhotoFlow.Functions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9b7f2444fe989aa327c5468c70c39e0a6ebf04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38377bfe3836900c8f66a2d1be80fa35b2e24fcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzurePhotoFlow.Functions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzurePhotoFlow.Functions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
