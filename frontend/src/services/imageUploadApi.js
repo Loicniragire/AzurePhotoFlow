9ebcf6d142cd9a0ca48d7f9220d4804997f6fd7a
loicniragire <loic.niragire@gmail.com>
2025-01-09 08:47:23 -0500

    Testing...

--- frontend/src/services/imageUploadApi.js
+++ frontend/src/services/imageUploadApi.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+// print all Environment Variables
+console.log('All Environment Variables:', import.meta.env);
+
 console.log('Mode:', import.meta.env.MODE);
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log('API_BASE_URL:', API_BASE_URL);
