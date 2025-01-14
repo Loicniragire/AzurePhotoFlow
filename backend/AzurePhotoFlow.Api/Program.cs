446bc938c7d1ae57230b28cedac672118dd76b7d
loicniragire <loic.niragire@gmail.com>
2025-01-14 14:30:39 -0500

    Testing...

--- backend/AzurePhotoFlow.Api/Program.cs
+++ backend/AzurePhotoFlow.Api/Program.cs
@@ -11,7 +11,28 @@ DotNetEnv.Env.Load();
 // Configure Kestrel for HTTP and HTTPS
 builder.WebHost.ConfigureKestrel(options =>
 {
+
+    string certDirectory = Directory.GetCurrentDirectory();
+
+    // Check if running in Docker
+    bool isInDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+    // Determine certificate path
+    string certFile = isInDocker
+        ? Environment.GetEnvironmentVariable("CERTIFICATE_PATH")
+		:Path.Combine(certDirectory, "..", "..", "certs", "https", "aspnetapp.pfx");
+    if (!File.Exists(certFile))
+    {
+        throw new FileNotFoundException("Certificate file not found", certFile);
+    }
+
     options.ListenAnyIP(8080); // HTTP
+    options.ListenAnyIP(443, listenOptions =>
+    {
+        string certPassword = Environment.GetEnvironmentVariable("CERTIFICATE_PASSWORD");
+		Console.WriteLine($"Certificate Password: {certPassword}");
+        listenOptions.UseHttps(certFile, certPassword);
+    }); // HTTPS
 });
 
 
