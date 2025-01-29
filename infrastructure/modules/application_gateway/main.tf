b4663657e39d15b5f7159f9c43634583c23e0579
loicniragire <loic.niragire@gmail.com>
2025-01-29 06:33:03 -0500

    Testing...

--- infrastructure/modules/application_gateway/main.tf
+++ infrastructure/modules/application_gateway/main.tf
@@ -56,6 +56,7 @@ resource "azurerm_application_gateway" "this" {
     interval            = 30
     timeout             = 30
     unhealthy_threshold = 3
+    protocol            = "Http"
   }
 
   # Backend HTTP Settings
