dc64c10081ae336b90b9c6f879fc8bfb60d95183
loicniragire <loic.niragire@gmail.com>
2025-01-30 10:15:48 -0500

    Testing...

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -154,7 +154,7 @@ resource "azurerm_linux_web_app" "web_app" {
 
 resource "azurerm_application_insights" "app_insights" {
   name                = "pf-app-insights"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   application_type    = "web"
 }
