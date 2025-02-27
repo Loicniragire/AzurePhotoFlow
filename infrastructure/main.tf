859c92ce3dcee4c3ed820040d7a1842f2a03afba
loicniragire <loic.niragire@gmail.com>
2025-02-27 13:43:17 -0500

    Testing...

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -255,7 +255,6 @@ resource "azurerm_monitor_diagnostic_setting" "function_app_diagnostics" {
   log_analytics_workspace_id = azurerm_log_analytics_workspace.log_workspace.id
 
   enabled_log { category = "FunctionAppLogs"}
-  enabled_log { category = "FunctionAppConsoleLogs"}
   metric { category = "AllMetrics"}
 }
 
