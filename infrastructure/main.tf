77d0b8b955909618815dfc5534e32369569a9639
loicniragire <loic.niragire@gmail.com>
2025-02-27 22:47:56 -0500

    Testing...

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -312,7 +312,7 @@ resource "azurerm_linux_function_app" "backend_function_app" {
     CosmosDBConnectionString = azurerm_cosmosdb_account.db.primary_sql_connection_string
     APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.app_insights.connection_string
     DOCKER_ENABLE_LOGGING  = "true"
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "true"
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
     FUNCTIONS_EXTENSION_VERSION = "~4"
     # Add any additional application settings here.
   }
