2864c675404855f1c7ff000a6121b86bd8659405
loicniragire <loic.niragire@gmail.com>
2025-03-11 09:02:54 -0400

    Testing...

--- backend/AzurePhotoFlow.Functions/Services/MetadataProcessor.cs
+++ backend/AzurePhotoFlow.Functions/Services/MetadataProcessor.cs
@@ -5,21 +5,26 @@ using Microsoft.Extensions.Logging;
 
 namespace Functions.Services;
 
-public class ImageMetadataProcessor: IMetadataProcessor
+public class ImageMetadataProcessor : IMetadataProcessor
 {
-	private const string DatabaseName = "loicportraits-cosmosdb";
-	private const string ContainerName = "MetadataContainer";
+    private const string DatabaseName = "loicportraits-cosmosdb";
+    private const string ContainerName = "MetadataContainer";
     private readonly Container _container;
     private readonly ILogger<ImageMetadataProcessor> _logger;
 
     public ImageMetadataProcessor(CosmosClient cosmosClient, ILogger<ImageMetadataProcessor> logger)
     {
-        _container = cosmosClient.GetDatabase(DatabaseName).GetContainer(ContainerName);
         _logger = logger;
-    }
+        var database = cosmosClient.GetDatabase(DatabaseName);
+        var containerResponse = database.CreateContainerIfNotExistsAsync(ContainerName, "/id")
+                                          .GetAwaiter().GetResult();
+        _container = containerResponse.Container;
 
+        _logger.LogInformation($"{nameof(ImageMetadataProcessor)} initialized. Container '{ContainerName}' is ready in database '{DatabaseName}'.");
+    }
     public async Task ProcessAsync(ImageMetadata metadata)
     {
+        _logger.LogInformation($"{nameof(ImageMetadataProcessor)} processing request");
         try
         {
             await _container.CreateItemAsync(metadata, new PartitionKey(metadata.Id));
